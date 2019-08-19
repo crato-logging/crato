9c7f5578750f9b8ca164b934dd32457af6983bdf
4g3m <13265214+z3r033@users.noreply.github.com>
2019-08-19 04:19:45 -0700

    fix bugs && more
    Co-authored-by: Kurth O'Connor <jkurthoconnor@gmail.com>
    Co-authored-by: Alex Soloviev <alex.soloviev@gmail.com>

--- cli.js
+++ cli.js
@@ -11,7 +11,9 @@ service            description
 -------            -----------
 rsyslog            Rsyslog Central Server
 zookeeper          Apache Zookeeper
-kafka(|2|3)        Kafka Broker 1, 2, or 3
+kafka              Kafka Broker 1
+kafka2             Kafka Broker 2
+kafka3             Kafka Broker 3
 influxdb           InfluxDB
 chronograf         Chronograf
 consumer           Kafka Consumer for AWS S3 & InfluxDB`
@@ -69,7 +71,7 @@ const displayDockerLogs = (service) => {
     service = service.toLowerCase()
     if (SERVICES.includes(service)) {
         log(`Displaying logs for ${NAMES[service]}...`)
-        exec(`docker-compose logs ${service}`).on('close', (err, stdout, stderr) => {
+        exec(`docker-compose logs ${service}`, (err, stdout, stderr) => {
             console.log(stdout)
         });
     } else {
@@ -79,7 +81,9 @@ const displayDockerLogs = (service) => {
 }
 
 const liveTail = () => {
-    const tailingDockerCmd = () => exec('docker-compose exec rsyslog tail -f /var/log/syslog')
+    const tailingDockerCmd = () => exec('docker-compose exec rsyslog tail -f /var/log/syslog', (err, stdout, stderr) => {
+      console.log(stdout)
+    })
     log('Crato will shortly be live tailing logs streaming into the system...')
     log(`Enter Ctrl-C to exit live-tail mode`)
     setTimeout(tailingDockerCmd, 2000)
@@ -92,14 +96,14 @@ const stopCrato = () => {
 
 const displaySystemStatus = () => (exec('docker ps', (err, stdout, stderr) => console.log(stdout)))
 
-const startService = (name) => {
-    name = name.toLowerCase
+const startService = (service) => {
+    service = service.toLowerCase()
     if (SERVICES.includes(service)) {
         log(`Starting ${NAMES[service]}...`)
-        exec(`docker-compose start ${service}`.on('close', () => {
+        exec(`docker-compose start ${service}`).on('close', () => {
             log(`${NAMES[service]} has successfully started`)
-        }))
-    } else if (!name) {
+        });
+    } else if (!service) {
         log("Starting up Crato...")
         deployCrato()
     } else {
@@ -108,14 +112,14 @@ const startService = (name) => {
     }
 }
 
-const stopService = (name) => {
-    name = name.toLowerCase()
+const stopService = (service) => {
+    service = service.toLowerCase()
     if (SERVICES.includes(service)) {
-        log(`Starting ${NAMES[service]}...`)
-        exec(`docker-compose stop ${service}`.on('close', () => {
+        log(`Stopping ${NAMES[service]}...`)
+        exec(`docker-compose stop ${service}`).on('close', () => {
             log(`${NAMES[service]} has been shut down`)
-        }))
-    } else if (!name) {
+        });
+    } else if (!service) {
         log("Shutting down Crato...")
         stopCrato()
     } else {
@@ -125,7 +129,36 @@ const stopService = (name) => {
 }
 
 const installKafka = () => {
+    log('Installing Kafka...');
+    exec('docker-compose up -d zookeeper', (err, stdout, stderr) => {
+        log('Starting Zookeeper...');
+        setTimeout(() => {
+            log('Starting Kafka Brokers...');
+            exec('docker-compose up -d kafka').on('close', () => {
+                log('1st Kafka Broker running');
+                exec('docker-compose up -d kafka2').on('close', () => {
+                    log('2nd Kafka Broker running');
+                    exec('docker-compose up -d kafka3').on('close', () => {
+                        log('3rd Kafka Broker running');
 
+                        setTimeout(() => {
+                            log("Setting up 'textlogs' & 'jsonlogs' topic...")
+                            exec('docker-compose exec kafka kafka-topics --create --zookeeper zookeeper:8092 --create --topic textlogs --replication-factor 3 --partitions 6 --if-not-exists').on('close', () => {
+                                log('textlogs topic created')
+                                exec('docker-compose exec kafka kafka-topics --create --zookeeper zookeeper:8092 --create --topic jsonlogs --replication-factor 3 --partitions 6 --if-not-exists').on('close', () => {
+                                    log('jsonlogs topic created')
+                                    exec('docker-compose stop');
+                                    log('Stopping Zookeeper and Kafka Brokers...')
+                                })
+                            })
+
+                        }, 7000);
+                    });
+                });
+            });
+            log('Kafka cluster created with 3 brokers!');
+        }, 7000);
+    });
 }
 
 
@@ -145,19 +178,22 @@ program.command('start <service>')
     .action((service) => startService(service))
 
 program.command('stop <service>')
-    .description('Stops Crato or a specific service')
-    .action((service) => startService(service))
+    .description('Stops a specific Crato service')
+    .action((service) => stopService(service))
 
 program.command('install-kafka')
-    .description("Installs Kafka cluster and jsonlogs & textlogs topics")
+    .description("Installs Kafka cluster and textlogs & jsonlogs topics")
     .action(installKafka)
 
 program.command('container-logs <service>')
-    .description(`Displays Docker container logs for a service`)
+    .alias('cl')
+    .description(`Displays Docker container logs for a specific service`)
     .action((service) => displayDockerLogs(service))
 
+
 program.command('live-tail')
-    .description('See all logs streaming into Crato. Press Ctrl-C to exit.')
+    .alias('lt')
+    .description('See all logs streaming into Crato. Press Ctrl-C to exit')
     .action(liveTail)
 
 program.command('status')
